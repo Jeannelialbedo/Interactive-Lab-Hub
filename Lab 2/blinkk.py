30b0c706eacbb89c445e79f6f298363b7ba58246
Jeanne Li <68198049+Jeannelialbedo@users.noreply.github.com>
2021-03-13 07:28:03 +0800

    Add files via upload

--- Lab 2/blinkk.py
+++ Lab 2/blinkk.py
@@ -4,7 +4,7 @@ import time
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
 GPIO.setup(21, GPIO.OUT) # output rf
 GPIO.output(21, True)
-time.sleep(2)
-GPIO.output(4, False)
+time.sleep(1)
+GPIO.output(21, False)
 print("clean up")
 GPIO.cleanup() # cleanup all GPIO
